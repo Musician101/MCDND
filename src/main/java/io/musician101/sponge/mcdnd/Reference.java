2ab043babe22fcfc4dd5e914910565bd0d15750b
Bruce <yellowfireproductions@yahoo.com>
2016-08-23 16:36:56 -0700

    Add private constructor.

--- src/main/java/io/musician101/sponge/mcdnd/Reference.java
+++ src/main/java/io/musician101/sponge/mcdnd/Reference.java
@@ -5,4 +5,9 @@ public class Reference
     public static final String ID = "io.musician101.mcdnd";
     public static final String NAME = "Minecraft Dungeons & Dragons";
     public static final String VERSION = "ALPHA-1.0";
+
+    private Reference()
+    {
+
+    }
 }
